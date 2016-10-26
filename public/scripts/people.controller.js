3b36c6f5440b51ce388982a88bac7304b22af814
elisa lee <elisa.my.lee@gmail.com>
2016-10-26 10:52:46 -0500

    add fields and sorting on get request

--- public/scripts/people.controller.js
+++ public/scripts/people.controller.js
@@ -6,18 +6,37 @@ function PeopleController($http) {
   var controller = this;
   controller.people = [];
 
-  controller.listPeople = function(){
-    console.log('Listing people');
-    $http.get('/people').then(function(response){
-      console.log('response', response);
-      controller.people = response.data;
-    }, function(error){
-      console.log('error making request', error);
-    });
+  controller.listPeople = function (option) {
+    if (option === 'hometown') {
+      $http.get('/people/hometown').then(function (response) {
+        console.log('response', response);
+        controller.people = response.data;
+      }, function (error) {
+
+        console.log('error making request', error);
+      });
+    } else if (option === 'movie') {
+      $http.get('/people/movie').then(function (response) {
+        console.log('response', response);
+        controller.people = response.data;
+      }, function (error) {
+
+        console.log('error making request', error);
+      });
+    } else {
+      console.log('Listing people');
+      $http.get('/people').then(function (response) {
+        console.log('response', response);
+        controller.people = response.data;
+      }, function (error) {
+
+        console.log('error making request', error);
+      });
+    }
   };
 
   controller.addPerson = function() {
-    var data = {name: controller.name};
+    var data = { name: controller.name, hometown: controller.hometown, movie: controller.movie };
 
     $http.post('/people', data).then(function(response){
       console.log('response', response);
--- routes/people.js
+++ routes/people.js
@@ -1,24 +1,42 @@
 const router = require('express').Router();
 const Person = require('../models/person');
 
-router.get('/', function(req, res){
+router.get('/hometown', function (req, res) {
   // dataFromTheDatabase is the list of documents that match the query
   // all the people in the database
-  Person.find({}).then(function(dataFromTheDatabase){
+  console.log('get req', req);
+  Person.find({}).sort({ hometown: 1 }).then(function (dataFromTheDatabase) {
     console.log('Documents from mongo', dataFromTheDatabase);
     res.send(dataFromTheDatabase);
   });
 });
 
-router.post('/', function(req, res){
+router.get('/movie', function (req, res) {
+  console.log('get req', req);
+  Person.find({}).sort({ movie: 1 }).then(function (dataFromTheDatabase) {
+    console.log('Documents from mongo', dataFromTheDatabase);
+    res.send(dataFromTheDatabase);
+  });
+});
+
+router.get('/', function (req, res) {
+  console.log('get req', req);
+  Person.find({}).sort({ name: 1 }).then(function (dataFromTheDatabase) {
+    console.log('Documents from mongo', dataFromTheDatabase);
+    res.send(dataFromTheDatabase);
+  });
+});
+
+router.post('/', function (req, res) {
   var name = req.body.name;
+  var hometown = req.body.hometown;
+  var movie = req.body.movie;
   console.log('name', name);
-  var personToSave = new Person({name: name});
-  personToSave.save().then(function(){
+  var personToSave = new Person({ name: name, hometown: hometown, movie: movie });
+  personToSave.save().then(function () {
     console.log('saved a new person');
     res.send(201);
   });
 });
-
 
 module.exports = router;
